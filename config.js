6ac63035bd870027bf84859821b428dfe88a6cf1
EmmsDevs <emman199810@gmail.com>
2023-02-13 01:54:05 +0800

    Fixed database connection

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var mysql = require('mysql2');
 
 exports.con = mysql.createConnection({
-  host: "localhost",
+  host: "18.191.54.215",
+  port:"3306",
   user: "hrasp",
   password:"1234",
   database:"hrasp"
